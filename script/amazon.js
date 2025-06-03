2789a0a231e931de7b19a2e640847e15ec4459b2
never-ever-n <kavadi.nikhil8909@gmail.com>
2025-06-03 17:30:02 +0530

    created a cart.js file and pushed data to the cart array

--- script/amazon.js
+++ script/amazon.js
@@ -1,4 +1,5 @@
 
+
 let productsgrid=document.querySelector(".products-grid")
 let product="";
 products.forEach((index)=>{
@@ -22,7 +23,7 @@ products.forEach((index)=>{
           </div>
 
           <div class="product-price">
-            $${(index.pricecents/100).toFixed(2)}
+            $${(index.priceCents/100).toFixed(2)}
           </div>
 
           <div class="product-quantity-container">
@@ -47,7 +48,10 @@ products.forEach((index)=>{
             Added
           </div>
 
-          <button class="add-to-cart-button button-primary">
+          <button class="add-to-cart-button button-primary js-add-to-cart" data-id="${index.id}"
+                data-name="${index.name}"
+                data-image="${index.image}"
+                data-pricecents="${index.priceCents}">
             Add to Cart
           </button>
         </div>`
@@ -55,3 +59,34 @@ products.forEach((index)=>{
 )
 console.log(product)
 productsgrid.innerHTML=product
+let count=0;
+document.querySelectorAll(".js-add-to-cart").forEach((button)=>{
+    button.addEventListener("click",(event)=>{
+        let ids=event.target.dataset.id;
+        let names=event.target.dataset.name;
+        let images=event.target.dataset.image;
+        let pricecents=event.target.dataset.pricecents;
+        cart.forEach((item)=>{
+            if(item.id==ids){
+              item.quantity++
+              count++
+            }
+        })
+        if(count==0){
+          cart.push({
+            id:ids,
+            name:names,
+            image:images,
+            pricecents:pricecents,
+            quantity:1,
+          })
+        }
+        let cartquantity=0
+        cart.forEach((item)=>{
+          cartquantity+=item.quantity
+        })
+        document.querySelector(".cart-quantity").innerHTML=cartquantity
+      count=0;
+      console.log(cart)
+    })
+})
